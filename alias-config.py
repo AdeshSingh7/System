8793a42d74b36026aff79069019f3643b6595cf4
Adesh Singh Rathore <72265093+AdeshSingh7@users.noreply.github.com>
2023-04-24 23:01:04 +0530

    Update alias-config.py

--- alias-config.py
+++ alias-config.py
@@ -23,7 +23,7 @@ try:
     with open(os.path.expanduser("~/.bashrc"), "a") as file:
         for alias in aliases:
             if not is_alias_present(alias):
-                file.write(f"\n{alias}")
+                file.write(f"{alias}")
     print(f"{GREEN}Aliases added successfully!{RESET}")
 except FileNotFoundError:print(f"{RED}Error: The .bashrc file could not be found.{RESET}")
 except PermissionError:print(f"{RED}Error: You do not have permission to modify the .bashrc file.{RESET}")
