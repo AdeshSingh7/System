2c0003b139548ab30a75485a1fbec3b6cbeaec4c
Adesh Singh Rathore <72265093+AdeshSingh7@users.noreply.github.com>
2023-04-30 18:45:29 +0530

    Update alias-config.py

--- alias-config.py
+++ alias-config.py
@@ -7,11 +7,12 @@ RESET = "\33[0m"
 
 # Define aliases
 aliases = [
-    "adesh=\"notify-send --urgency=low 'Welcome Back' 'Mr. Adesh Singh'\"",
-    "banner=\"python3 ~/Python-Projects/System/banner.py\"",
-    "panel=\"python3 ~/Python-Projects/ParkPlus/panel-config.py\"",
-    "server=\"python3 ~/Python-Projects/ParkPlus/server-config.py\"",
-    "clear=\"banner\""
+    "alias adesh=\"notify-send --urgency=low 'Welcome Back' 'Mr. Adesh Singh'\"",
+    "alias banner=\"python3 ~/Python-Projects/System/banner.py\"",
+    "alias meta=\"python3 ~/Python-Projects/Parkplus/metadata.py\"",
+    "alias panel=\"python3 ~/Python-Projects/ParkPlus/panel-config.py\"",
+    "alias server=\"python3 ~/Python-Projects/ParkPlus/server-config.py\"",
+    "alias clear=\"banner\""
 ]
 
 # Check if alias is already present in .bashrc file
@@ -24,7 +25,7 @@ try:
     with open(os.path.expanduser("~/.bashrc"), "a") as file:
         for alias in aliases:
             if not is_alias_present(alias):
-                file.write(f"alias {alias}\n")
+                file.write(f"{alias}\n")
     print(f"{GREEN}[+] Aliases added successfully!{RESET}")
 except FileNotFoundError:print(f"{RED}[!] Error: The .bashrc file could not be found.{RESET}")
 except PermissionError:print(f"{RED}[!] Error: You do not have permission to modify the .bashrc file.{RESET}")
